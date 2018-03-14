bb25c7923ed111864b3360be12bd62947cb2cffd
zhangpanpan <zhangpanpan@qianshengqian.com>
2018-03-14 10:03:25 +0800

    half close

--- ch8/netcat3/netcat.go
+++ ch8/netcat3/netcat.go
@@ -10,6 +10,8 @@ import (
 //!+
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8000")
+	tcpCon := conn.(*net.TCPConn)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,7 +22,7 @@ func main() {
 		done <- struct{}{} // signal the main goroutine
 	}()
 	mustCopy(conn, os.Stdin)
-	conn.Close()
+	tcpCon.CloseWrite()
 	<-done // wait for background goroutine to finish
 }
 
